337547791beeef2205422de84c5e38bee5506cf8
Dalton Marler <daltonmarler@outlook.com>
2020-11-28 16:06:12 -0600

    Removed unused code, added icon

--- cafebruh.js
+++ cafebruh.js
@@ -1,15 +1,14 @@
-// jQuery("body").children().each(function() {
-//     jQuery(this).html(jQuery(this).html().replace('Cardboard Corner', 'Cafe Bruh'));
-// });
-
-// page = jQuery("html").html();
-
-// jQuery("html").html(page.replace(/Cardboard Corner/g, 'Cafe Bruh'));
-
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a, rs-layer, title, head');
 
 for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.includes('Cardboard Corner')) {
         text[i].innerHTML = text[i].innerHTML.replace(/Cardboard Corner/g, 'Cafe Bruh');
+    }
+}
+
+const addresses = document.querySelectorAll('footer a');
+for (let i = 0; i < addresses.length; i++) {
+    if (addresses[i].innerHTML.includes('cardboardcorner')) {
+        addresses[i].innerHTML = addresses[i].text.replace(/cardboardcorner/g, 'cafebruh');
     }
 }
